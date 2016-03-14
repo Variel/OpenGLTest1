792ead5b06c6b9f361aa5eb77d33f4e73c68fda6
variel <blackmagic2@naver.com>
2016-03-14 15:41:16 +0900

    삼각형의 한 변의 z축 시간에 따라 변하도록

--- OpenGLTest1/main.cpp
+++ OpenGLTest1/main.cpp
@@ -2,22 +2,53 @@
 #include <gl/GL.h>
 #include <gl/GLU.h>
 
+float z = 0;
+float factor = 1.0f;
+float speed = 5.0f;
+float zMin = -5;
+float zMax = 5;
+int lastTime = 0;
+
 void render() {
+	int elapsedTime = glutGet(GLUT_ELAPSED_TIME);
+	int gap = lastTime - elapsedTime;
+	lastTime = elapsedTime;
+
+	z += factor * speed * (float) gap / 1000;
+	if (z < zMin) {
+		z = zMin * 2 - z;
+		factor *= -1;
+	}
+
+	if (z > zMax) {
+		z = zMax * 2 - z;
+		factor *= -1;
+	}
+
 	glClearColor(100.0f / 255, 149.0f / 255, 237.0f / 255, 0);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();
 
 	glLineWidth(100.0f);
-	glBegin(GL_LINE_LOOP);
+	gluPerspective(45.0, 1.0, 0.1, 1000);
+	gluLookAt(0, 0, -10, 0, 0, 0, 0, 1, 0);
+
+	glBegin(GL_TRIANGLES);
 	{
 		glColor3f(1.0f, 0, 0);
+
 		glVertex3f(0.0f, 0.5f, 0.0f);
 		glVertex3f(-0.5f, -0.5f, 0.0f);
-		glVertex3f(0.5f, -0.5f, 0.0f);
+		glVertex3f(0.5f, -0.5f, z);
 	}
 	glEnd();
 
 	glFlush();
+}
+
+void timerCallback(int mills) {
+	glutTimerFunc(mills, timerCallback, mills);
+	glutPostRedisplay();
 }
 
 int main(int argc, char* argv[]) {
@@ -27,6 +58,7 @@ int main(int argc, char* argv[]) {
 	glutInitWindowSize(640, 640);
 	glutCreateWindow("OpenGL TEST");
 
+	glutTimerFunc(16, timerCallback, 16);
 	glutDisplayFunc(render);
 
 	glutMainLoop();
